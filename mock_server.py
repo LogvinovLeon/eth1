af8962992083e31511fa211d3dd4d16d51c37ac8
Leonid Logvinov <logvinov.leon@gmail.com>
2016-02-23 14:39:01 +0100

    Improve mock server
    Set reuse_adress option to prevent "already in use" error
    Add arguments parsing
    Add dumb logging

--- mock_server.py
+++ mock_server.py
@@ -1,16 +1,29 @@
+import argparse
 import SocketServer
+import socket
 from time import sleep
+
+
+class MyTCPServer(SocketServer.TCPServer):
+    def server_bind(self):
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind(self.server_address)
 
 
 class MyTCPHandler(SocketServer.StreamRequestHandler):
     def handle(self):
         while True:
             self.wfile.write("Hello\nMulti line\n")
+            print "Write finished"
             sleep(1)
 
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 80
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    parser = argparse.ArgumentParser(description='Starts dummy TCP server accepting connections '
+                                                 'and writing multiline data.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--host', default="localhost", type=str)
+    parser.add_argument('--port', default=80, type=int)
+    args = parser.parse_args()
+    server = MyTCPServer((args.host, args.port), MyTCPHandler)
     server.serve_forever()
-
