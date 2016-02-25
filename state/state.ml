d4b59e6d0fa5762332c91d2287c9be58a16a6f16
Leonid Logvinov <logvinov.leon@gmail.com>
2016-02-25 15:41:01 +0100

    Add assets with update_assets function to state

--- state/state.ml
+++ state/state.ml
@@ -1,8 +1,28 @@
+open Core.Std;;
 open Sexplib.Std;;
+open Types;;
 
 type t = {(* TODO *)
-    offers : int list
+    offers : int list;
+    assets : (symbol_t * size_t) list;
 } with sexp;;
 
-let initial = {offers = []};;
+let initial = {
+    offers = [];
+    assets = [
+        BOND, 0;
+        VALBZ, 0;
+        VALE, 0;
+        GS, 0;
+        MS, 0;
+        WFC, 0;
+        XLF, 0;
+    ]
+}
 
+let update_assets state symbol dir size =
+    let size = size * match dir with | Buy -> 1 | Sell -> -1 in
+    let assets = state.assets in
+    let open List.Assoc in
+    let value = find_exn assets symbol in
+    {state with assets = add (List.Assoc.remove state.assets symbol) symbol (value + size)};;
--- state/state.mli
+++ state/state.mli
@@ -1,3 +1,10 @@
-type t with sexp;;
+open Types;;
+
+type t = {
+    offers : int list;
+    assets : (symbol_t * size_t) list;
+} with sexp;;
 
 val initial : t;;
+
+val update_assets : t -> symbol_t -> dir_t -> int -> t
